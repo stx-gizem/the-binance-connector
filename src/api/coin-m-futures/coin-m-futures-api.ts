9dee45c3fb5f7200981561dc2e45a33937658c9f
stx-gizem <gizem@socialtradex.com>
2022-10-27 13:33:17 +0300

    Fix build errors

--- src/api/coin-m-futures/coin-m-futures-api.ts
+++ src/api/coin-m-futures/coin-m-futures-api.ts
@@ -1,15 +1,15 @@
 import { ApiBase } from "../../api-base";
 import { ConfigOptions } from "../../@types/config/options";
-import { Method } from "axios";
+import { AxiosResponse, Method } from "axios";
 import { ApiUrl } from "../../api-url";
 
 export class CoinMFuturesApi extends ApiBase {
 
-  protected publicRequest<R = any>(method: Method, path: string, params: {} = {}, config?: ConfigOptions, endpoint: ApiUrl = ApiUrl.DAPI): Promise<R> {
+  protected publicRequest<R = any>(method: Method, path: string, params: {} = {}, config?: ConfigOptions, endpoint: ApiUrl = ApiUrl.DAPI): Promise<AxiosResponse> {
     return super.publicRequest(method, path, params, config, endpoint);
   }
 
-  protected signRequest<R = any>(method: Method, path: string, params: {} = {}, config?: ConfigOptions, endpoint: ApiUrl = ApiUrl.DAPI): Promise<R> {
+  protected signRequest<R = any>(method: Method, path: string, params: {} = {}, config?: ConfigOptions, endpoint: ApiUrl = ApiUrl.DAPI): Promise<AxiosResponse> {
     return super.signRequest(method, path, params, config, endpoint);
   }
 }
